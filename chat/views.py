2cb672c87689eaf49e80811d3eb7999573910ad7
cristhian Villamarin <cristhianvillamarin2002@gmail.com>
2023-11-28 16:07:01 -0500

    viista

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .chatbot import response
 from .pdf import pdf
-
 from .pdf import convert_pdf_to_txt
 from django.shortcuts import render, redirect
+
+
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
@@ -20,4 +21,39 @@ def index(request):
 
 
 def pdf(request):
+    if request.method == 'POST':
+        # Obtener archivos PDF del formulario
+        pdf_files = request.FILES.getlist('pdf_files')
+
+        # Aquí puedes procesar los archivos según tus necesidades
+        for pdf_file in pdf_files:
+            # Procesar cada archivo, por ejemplo, guardarlo en el sistema de archivos
+            st.file_uploader(
+                "Upload document",
+                type=["pdf"],
+                key="file_uploader",
+                on_change=read_and_save_file(),
+                label_visibility="collapsed",
+                accept_multiple_files=True,
+            )
+            # o realizar alguna otra operación.
+
+        # Devolver alguna respuesta JSON si es necesario
+        return JsonResponse({'message': 'Archivos PDF recibidos correctamente.'})
+
     return render(request, "pdf.html")
+
+def read_and_save_file():
+    st.session_state["agent"].forget()  # to reset the knowledge base
+    st.session_state["messages"] = []
+    st.session_state["user_input"] = ""
+
+    for file in st.session_state["file_uploader"]:
+        with tempfile.NamedTemporaryFile(delete=False) as tf:
+            tf.write(file.getbuffer())
+            file_path = tf.name
+
+        with st.session_state["ingestion_spinner"], st.spinner(f"Ingesting {file.name}"):
+            st.session_state["agent"].ingest(file_path)
+        os.remove(file_path)
+
